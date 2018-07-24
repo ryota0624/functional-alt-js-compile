cbc47a5c3eaefd6c6a5d6bf548b9961223bc8643
ryota suzuki <ryota@fringe81.com>
2018-07-24 13:42:27 +0900

    テンプレートの作成

---
+++ elm/src/Mod105.elm
@@ -0,0 +1,42 @@
+module Mod105 exposing (..)
+
+get0: String
+get0 = "0"
+
+
+get1: String
+get1 = "1"
+
+
+get2: String
+get2 = "2"
+
+
+get3: String
+get3 = "3"
+
+
+get4: String
+get4 = "4"
+
+
+get5: String
+get5 = "5"
+
+
+get6: String
+get6 = "6"
+
+
+get7: String
+get7 = "7"
+
+
+get8: String
+get8 = "8"
+
+
+get9: String
+get9 = "9"
+
+  
